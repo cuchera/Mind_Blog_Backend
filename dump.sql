edcb4f12b8515ec9f450ef9182cb147c428fd99d
Cuchera <gabrielcuchera23@hotmail.com>
2026-05-16 13:57:07 -0300

    corrigindo algumas configurações

--- dump.sql
+++ dump.sql
@@ -11,14 +11,12 @@ CREATE TABLE users (
 
 CREATE TABLE posts (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
-    banner_image VARCHAR(255),
-    author_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    banner LONGBLOB,
+    banner_mime VARCHAR(100),
+    published_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    CONSTRAINT fk_post_author
-        FOREIGN KEY (author_id)
-        REFERENCES users(id)
-        ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
